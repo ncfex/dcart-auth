df2b8670aaaff5a6da1f6e0c43bc1b81aa5d6649
oguzhang <oguzhanguldibi@gmail.com>
2024-11-14 14:20:32 +0300

    update interfaces

--- internal/core/ports/inbound/service.go
+++ internal/core/ports/inbound/service.go
@@ -9,20 +9,23 @@ import (
 	userDomain "github.com/ncfex/dcart-auth/internal/core/domain/user"
 )
 
-type UserAuthenticator interface {
+type AuthenticationService interface {
 	Register(ctx context.Context, username string, password string) (*userDomain.User, error)
 	Login(ctx context.Context, username string, password string) (*tokenDomain.TokenPair, error)
 	Refresh(ctx context.Context, token string) (*tokenDomain.TokenPair, error)
 	Logout(ctx context.Context, token string) error
 }
 
-type PasswordEncrypter interface {
+type PasswordHasher interface {
 	Hash(password string) (string, error)
 	Compare(hashedPassword, password string) error
 }
 
-// TODO - more generic interface handle jwt,hex etc.
-type TokenManager interface {
-	Make(userID *uuid.UUID, expiresIn time.Duration) (string, error)
+type TokenGenerator interface {
+	Generate(userID *uuid.UUID, expiresIn time.Duration) (string, error)
 	Validate(token string) (*uuid.UUID, error)
+}
+
+type RefreshTokenGenerator interface {
+	Generate() (string, error)
 }
