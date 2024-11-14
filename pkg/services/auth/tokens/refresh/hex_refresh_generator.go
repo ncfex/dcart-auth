709876145330ef2febed336dc17b37716f282a42
oguzhang <oguzhanguldibi@gmail.com>
2024-11-14 15:09:23 +0300

    update refresh token generator

--- pkg/services/auth/tokens/refresh/hex_refresh_generator.go
+++ pkg/services/auth/tokens/refresh/hex_refresh_generator.go
@@ -11,29 +11,25 @@ var (
 	ErrTokenGenerationFailed = errors.New("token generation failed")
 )
 
-type HexTokenService interface {
-	Make() (string, error)
-}
-
-type service struct {
+type hexRefreshGenerator struct {
 	prefix      string
 	tokenLength int
 }
 
-func NewHexTokenService(prefix string, tokenLength int) HexTokenService {
-	return &service{
+func NewHexRefreshGenerator(prefix string, tokenLength int) *hexRefreshGenerator {
+	return &hexRefreshGenerator{
 		prefix:      prefix,
 		tokenLength: tokenLength,
 	}
 }
 
-func (s *service) Make() (string, error) {
-	token := make([]byte, s.tokenLength)
+func (g *hexRefreshGenerator) Generate() (string, error) {
+	token := make([]byte, g.tokenLength)
 	_, err := rand.Read(token)
 	if err != nil {
 		return "", ErrTokenGenerationFailed
 	}
 
-	tokenString := fmt.Sprintf("%s%s", s.prefix, hex.EncodeToString(token))
+	tokenString := fmt.Sprintf("%s%s", g.prefix, hex.EncodeToString(token))
 	return tokenString, nil
 }
