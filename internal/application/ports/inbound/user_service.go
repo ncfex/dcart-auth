46feb6056cb7677ecd7152f152f4c105f0b7b0d8
oguzhang <oguzhanguldibi@gmail.com>
2024-11-27 01:47:59 +0300

    update user service

--- internal/application/ports/inbound/user_service.go
+++ internal/application/ports/inbound/user_service.go
@@ -1,10 +1,13 @@
 package inbound
 
 import (
+	"context"
+
 	userDomain "github.com/ncfex/dcart-auth/internal/domain/user"
 )
 
 type UserSevice interface {
-	CreateUser(username, password string) (*userDomain.User, error)
-	ComparePassword(hashedPassword, password string) error
+	CreateUser(ctx context.Context, username, password string) (*userDomain.User, error)
+	ValidateWithCreds(ctx context.Context, username, password string) (*userDomain.User, error)
+	ValidateWithID(ctx context.Context, userID string) (*userDomain.User, error)
 }
--- internal/application/services/user_service.go
+++ internal/application/services/user_service.go
@@ -1,7 +1,9 @@
 package services
 
 import (
-	"github.com/google/uuid"
+	"context"
+	"fmt"
+
 	"github.com/ncfex/dcart-auth/internal/application/ports/inbound"
 	"github.com/ncfex/dcart-auth/internal/application/ports/outbound"
 	userDomain "github.com/ncfex/dcart-auth/internal/domain/user"
@@ -9,37 +11,69 @@ import (
 
 type userService struct {
 	passwordHasher outbound.PasswordHasher
+	userRepo       outbound.UserRepository
 }
 
 func NewUserService(
 	passwordHasher outbound.PasswordHasher,
+	userRepo outbound.UserRepository,
 ) inbound.UserSevice {
 	return &userService{
 		passwordHasher: passwordHasher,
+		userRepo:       userRepo,
 	}
 }
 
-func (s *userService) CreateUser(username, password string) (*userDomain.User, error) {
-	newID := uuid.New().String()
-	user, err := userDomain.NewUser(newID, username, password)
+func (s *userService) CreateUser(ctx context.Context, username, password string) (*userDomain.User, error) {
+	_, err := s.userRepo.GetUserByUsername(ctx, username)
+	if err == nil {
+		return nil, fmt.Errorf("get user username: %w", userDomain.ErrUserAlreadyExists)
+	}
+	if err != userDomain.ErrUserNotFound {
+		return nil, fmt.Errorf("get user username: %w", err)
+	}
+
+	user, err := userDomain.NewUser(username, password)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("new user: %w", err)
 	}
 
 	hashedPassword, err := s.passwordHasher.Hash(password)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("password hash: %w", err)
 	}
+
 	user.SetHashedPassword(hashedPassword)
 
+	if err := s.userRepo.CreateUser(ctx, user); err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
 	return user, nil
 }
 
-func (s *userService) ComparePassword(hashedPassword, password string) error {
-	err := s.passwordHasher.Compare(hashedPassword, password)
+func (s *userService) ValidateWithCreds(ctx context.Context, username, password string) (*userDomain.User, error) {
+	user, err := s.userRepo.GetUserByUsername(ctx, username)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("get user username: %w", err)
 	}
 
+	if err := s.comparePassword(user.PasswordHash, password); err != nil {
+		return nil, fmt.Errorf("compare password: %w", err)
+	}
+	return user, nil
+}
+
+func (s *userService) ValidateWithID(ctx context.Context, userID string) (*userDomain.User, error) {
+	user, err := s.userRepo.GetUserByID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("get user id: %w", err)
+	}
+	return user, nil
+}
+
+func (s *userService) comparePassword(hashedPassword, password string) error {
+	if err := s.passwordHasher.Compare(hashedPassword, password); err != nil {
+		return fmt.Errorf("compare: %w", err)
+	}
 	return nil
 }
