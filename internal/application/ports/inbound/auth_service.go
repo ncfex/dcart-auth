7b8567cf5c2394506e0a8ab6d77d30321fbfe5b7
oguzhang <oguzhanguldibi@gmail.com>
2024-11-28 14:26:40 +0300

    update auth service port

--- internal/application/ports/inbound/auth_service.go
+++ internal/application/ports/inbound/auth_service.go
@@ -2,15 +2,12 @@ package inbound
 
 import (
 	"context"
-
-	tokenDomain "github.com/ncfex/dcart-auth/internal/domain/token"
-	userDomain "github.com/ncfex/dcart-auth/internal/domain/user"
 )
 
 type AuthenticationService interface {
-	Register(ctx context.Context, username string, password string) (*userDomain.User, error)
-	Login(ctx context.Context, username string, password string) (*tokenDomain.TokenPair, error)
-	Refresh(ctx context.Context, tokenString string) (*tokenDomain.TokenPair, error)
-	Logout(ctx context.Context, tokenString string) error
-	Validate(ctx context.Context, tokenString string) (*userDomain.User, error)
+	Register(ctx context.Context, req RegisterRequest) (*UserResponse, error)
+	Login(ctx context.Context, req LoginRequest) (*TokenPairResponse, error)
+	Refresh(ctx context.Context, req RefreshRequest) (*TokenPairResponse, error)
+	Logout(ctx context.Context, req LogoutRequest) error
+	Validate(ctx context.Context, req ValidateRequest) (*ValidateResponse, error)
 }
