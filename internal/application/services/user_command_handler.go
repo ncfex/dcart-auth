574ce4a112139f5c5f8544b96dbe9f95d042842c
oguzhang <oguzhanguldibi@gmail.com>
2024-11-30 01:09:03 +0300

    add cmd/query handlers

---
+++ internal/application/services/user_command_handler.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"context"
+
+	"github.com/ncfex/dcart-auth/internal/application/commands"
+	"github.com/ncfex/dcart-auth/internal/application/ports/outbound"
+	userDomain "github.com/ncfex/dcart-auth/internal/domain/user"
+)
+
+type userCommandHandler struct {
+	eventStore outbound.EventStore // separate read/write
+}
+
+func NewUserCommandHandler(eventStore outbound.EventStore) *userCommandHandler {
+	return &userCommandHandler{
+		eventStore: eventStore,
+	}
+}
+
+func (h *userCommandHandler) HandleRegisterUser(ctx context.Context, cmd commands.RegisterUserCommand) (*userDomain.User, error) {
+	user, err := userDomain.NewUser(cmd.Username, cmd.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := h.eventStore.SaveEvents(ctx, user.GetID(), user.GetUncommittedChanges()); err != nil {
+		return nil, err
+	}
+
+	user.ClearUncommittedChanges()
+	return user, nil
+}
---
+++ internal/application/services/user_query_handler.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"context"
+
+	"github.com/ncfex/dcart-auth/internal/application/ports/outbound"
+	"github.com/ncfex/dcart-auth/internal/application/queries"
+	userDomain "github.com/ncfex/dcart-auth/internal/domain/user"
+)
+
+type userQueryHandler struct {
+	eventStore outbound.EventStore
+}
+
+func NewUserQueryHandler(eventStore outbound.EventStore) *userQueryHandler {
+	return &userQueryHandler{
+		eventStore: eventStore,
+	}
+}
+
+func (h *userQueryHandler) HandleGetUserById(ctx context.Context, query queries.GetUserByIDQuery) (*userDomain.User, error) {
+	events, err := h.eventStore.GetEvents(ctx, query.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(events) == 0 {
+		return nil, userDomain.ErrUserNotFound
+	}
+
+	user := &userDomain.User{}
+	for _, event := range events {
+		user.Apply(event)
+	}
+
+	return user, nil
+}
+
+func (h *userQueryHandler) HandleGetUserByUsername(ctx context.Context, query queries.GetUserByUsernameQuery) (*userDomain.User, error) {
+	events, err := h.eventStore.GetEventsByUsername(ctx, query.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(events) == 0 {
+		return nil, userDomain.ErrUserNotFound
+	}
+
+	user := &userDomain.User{}
+	for _, event := range events {
+		user.Apply(event)
+	}
+
+	return user, nil
+}
