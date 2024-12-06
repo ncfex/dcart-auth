6972122a10d670a5221be896c18bd4653c1809a8
oguzhang <oguzhanguldibi@gmail.com>
2024-12-06 23:54:34 +0300

    add event publisher

---
+++ internal/adapters/secondary/messaging/mqtest/publisher.go
@@ -0,0 +1,21 @@
+package mqtest
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ncfex/dcart-auth/internal/domain/shared"
+)
+
+type EventPublisher struct {
+}
+
+func NewEventPublisher() *EventPublisher {
+	return &EventPublisher{}
+}
+
+func (ep *EventPublisher) PublishEvent(ctx context.Context, event shared.Event) error {
+	fmt.Printf("%s event published for aggregate ID: %s\naggregate type: %s\n at time %s", event.GetEventType(), event.GetAggregateID(), event.GetAggregateType(), event.GetTimestamp().UTC())
+
+	return nil
+}
